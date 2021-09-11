2e5ead56bcbaa1bf1acaee6f84b47588ebe484a3
ASHIF ALI <63405841+ashif4ai@users.noreply.github.com>
2021-09-11 13:50:20 +0530

    ASHIF ALI
    User UI and UX Improved

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const taskInput = document.getElementById("task-input"),
+ 	tasktitle = document.getElementById("task-title"),
 	addTask = document.getElementById("task-add"),
-	taskList = document.getElementById("task-list");
+	taskList = document.getElementById("task-list"),
+	taskbutton = document.getElementById("task-btn"),
+	taskwrapper = document.getElementById("task-wrapper"),
+	taskform = document.getElementById("task-form");
+//
 
-function createTaskItem(task) {
-	let li = document.createElement("li");
-	li.classList.add("task");
-	li.innerText = task;
+taskbutton.addEventListener("click", function () {
+	taskwrapper.style.display = "none";
+	taskform.style.display = "block";
+});
 
-	taskList.appendChild(li);
+function createTaskItem(taskTitle,task) {
+
+	let taskcard = document.createElement("div");
+	let taskTit = document.createElement("div");
+	let taskDisc = document.createElement("p");
+
+	taskcard.classList.add("taskcard");
+	taskTit.classList.add("taskTitle");
+	taskDisc.classList.add("taskDisc");
+	
+	taskTit.innerText = taskTitle;
+	taskDisc.innerText = task;
+
+	taskList.appendChild(taskcard);
+	taskcard.appendChild(taskTit);
+	taskcard.appendChild(taskDisc);
 }
 
 addTask.addEventListener("click", function () {
-	createTaskItem(taskInput.value);
+	createTaskItem(tasktitle.value,taskInput.value);
+	tasktitle.value = "";
 	taskInput.value = "";
+	taskwrapper.style.display = "block";
+	taskform.style.display = "none";
 });
